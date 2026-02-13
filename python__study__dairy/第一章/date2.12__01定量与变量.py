f7b2442aeec76b54b34ec7e3b2046754db3d6287
yangmingzhou2759678997 <2759678997@qq.com>
2026-02-13 23:15:03 +0800

    2月12日python学习记录

---
+++ python__study__dairy/第一章/date2.12__01定量与变量.py
@@ -0,0 +1,26 @@
+#定量输出
+from operator import truediv
+
+print("#########################")
+print("######## 白日依山尽 #######")
+print("######## 黄河入海流 #######")
+print("######## 欲穷千里目 #######")
+print("######## 更上一层楼 #######")
+print("#########################")
+
+
+#变量输出(变量定义时必须赋值才能使用,可一次定义多个变量)
+num= 11306
+print(num)
+
+num = num + 1
+print(num)
+
+num = True
+print(num)
+
+#举例
+base = 20.4
+up = 48
+print("最终数量=base+up*2")
+print("最终数量 =",base+up*2)
